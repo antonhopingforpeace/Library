e07c3c6c70ad3459bc6d65ae1b5adb7ca3bf44ca
antonhopingforpeace <116687553+antonhopingforpeace@users.noreply.github.com>
2024-07-14 01:09:50 +0300

    Make the program work with basic styling

--- script.js
+++ script.js
@@ -23,19 +23,10 @@ function addBookToLibrary(book){
     displayBooksInLibrary();
 }
 
-// let deleteButton = document.querySelector(".delete");
-// deleteButton.addEventListener("click",()=>{
-//     myLibrary.splice(deleteButton.getAttribute("index")-1,1);
-// })
-
-
-// const display = document.querySelector(".display-books");
-// display.addEventListener("click",displayBooksInLibrary);
-
 const dialog = document.querySelector("dialog");
 const showDialogButton = document.querySelector(".new-book");
 const closeDialogButton = document.querySelector(".close-dialog");
-const confirmDialogButton = document.querySelector(".confirm-dialog")
+const confirmDialogButton = document.querySelector(".confirm-dialog");
 
 showDialogButton.addEventListener("click", ()=>{
     dialog.showModal();
@@ -59,15 +50,6 @@ confirmDialogButton.addEventListener("click", (event)=>{
     
 })
 
-
-// function displayBooksInLibrary(){
-//     myLibrary.forEach((book)=>{
-//         let h2 = document.createElement("h2");
-//         h2.textContent = book.info();
-//         container.appendChild(h2);
-//     })
-// }
-
 function displayBooksInLibrary(){
     
     for(let i = 0; i< myLibrary.length; i++){
@@ -86,9 +68,10 @@ function displayBooksInLibrary(){
             container.innerHTML="";
             displayBooksInLibrary();
         });
-        placementOfBook.appendChild(deleteButton);
+        
 
         let readButton = document.createElement("button");
+        readButton.classList.add("read");
         readButton.textContent="READ";
         readButton.addEventListener("click",()=>{
             if(myLibrary[i].read==="yes"){
@@ -99,7 +82,8 @@ function displayBooksInLibrary(){
             }
                 h2.textContent = myLibrary[i].info();
         });
-        placementOfBook.appendChild(readButton)
+        placementOfBook.appendChild(readButton);
+        placementOfBook.appendChild(deleteButton);
         
         container.appendChild(placementOfBook);
     }
